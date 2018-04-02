b61ede8453e00f1ad6f27c6b92662073cca92571
Bassel A. Elerian <basselakmal@gmail.com>
2018-04-02 22:09:13 +0200

    Update webcrawler.sql

--- webcrawler.sql
+++ webcrawler.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.2
--- http://www.phpmyadmin.net
+-- version 4.0.10.18
+-- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 31, 2018 at 12:21 AM
+-- Generation Time: Apr 02, 2018 at 08:07 PM
 -- Server version: 5.6.13
 -- PHP Version: 5.4.17
 
@@ -19,20 +19,18 @@ SET time_zone = "+00:00";
 --
 -- Database: `webcrawler`
 --
-CREATE DATABASE IF NOT EXISTS `webcrawler` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `webcrawler`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `crawledpages`
+-- Table structure for table `crawled_pages`
 --
 
-CREATE TABLE IF NOT EXISTS `crawledpages` (
-  `domainURL` varchar(300) NOT NULL,
-  `Title` varchar(300) NOT NULL,
-  `Keywords` varchar(700) NOT NULL,
-  `Description` varchar(700) NOT NULL,
+CREATE TABLE IF NOT EXISTS `crawled_pages` (
+  `domainURL` varchar(500) NOT NULL,
+  `isIndexed` tinyint(1) NOT NULL DEFAULT '0',
+  `highPriority` tinyint(1) NOT NULL DEFAULT '0',
+  `isCrawled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`domainURL`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -45,18 +43,30 @@ CREATE TABLE IF NOT EXISTS `crawledpages` (
 CREATE TABLE IF NOT EXISTS `domain_referrer` (
   `domainURL` varchar(500) NOT NULL,
   `referrerURL` varchar(500) NOT NULL,
-  `isIndexed` tinyint(1) NOT NULL DEFAULT '0',
-  `isCrawled` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`domainURL`,`referrerURL`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `invertedfile`
+-- Table structure for table `indexed_pages`
 --
 
-CREATE TABLE IF NOT EXISTS `invertedfile` (
+CREATE TABLE IF NOT EXISTS `indexed_pages` (
+  `domainURL` varchar(300) NOT NULL,
+  `Title` varchar(300) NOT NULL,
+  `Keywords` varchar(700) NOT NULL,
+  `Description` varchar(700) NOT NULL,
+  PRIMARY KEY (`domainURL`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inverted_file`
+--
+
+CREATE TABLE IF NOT EXISTS `inverted_file` (
   `Token` varchar(500) NOT NULL,
   `Count` int(11) NOT NULL,
   `URL` varchar(500) NOT NULL,
