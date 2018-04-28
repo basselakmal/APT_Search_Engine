17bd99a43e7b7992e48e20bab9cb4beb45fee28f
Bassel A. Elerian <basselakmal@gmail.com>
2018-04-28 21:00:24 +0200

    Update APTServlet.java

--- NetBeans/APT - Search Engine/src/java/APTServlet.java
+++ NetBeans/APT - Search Engine/src/java/APTServlet.java
@@ -6,6 +6,10 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Vector;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,7 +29,7 @@ public class APTServlet extends HttpServlet {
             throws ServletException, IOException {
         
         //Fady
-        Vector<String> Tokens = getTokens(request);
+        Vector<String> Tokens = getTokens(request);           
         
         //Bassel
         DB_Manager DB_Man = new DB_Manager();
@@ -60,12 +64,51 @@ public class APTServlet extends HttpServlet {
     
     private Vector<String> getTokens(HttpServletRequest request){
         //Extract search words from the request, stem them and then return an array of strings that correspond to the tokens.
-        
+        String[] stopWordsArr = {"", " ", "a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"};
+        HashSet<String> stopWords = new HashSet<>(Arrays.asList(stopWordsArr));
         Vector<String> Tokens = new Vector<String>();
-        
-        //Just for testing!
-        Tokens.add("donald");
-        Tokens.add("trump");
+        String searchQuery = request.getParameter("searchQuery");
+        if (searchQuery != null)
+        {   
+            String query = searchQuery.toLowerCase(); //make sure that they are in lower case
+            
+            //First :- Phrase Search => check if there is bracket at the beginning and end of the entered word
+            if (query.startsWith("\"") && query.endsWith("\""))
+            {
+                query = query.substring(1,query.length()-1); //remove the quotations from the input
+                //put the elements of in input in order and retrieve from the database in order 
+                //example :- "Search Engine Implementation"
+                //Get => Search followerd by Engine followed by Implementation
+                query = query.replaceAll("[^a-zA-Z ]|(  )+", "");           
+                String[] BodyArray = query.split(" ");                      // split words
+                
+                for(String Token : BodyArray)
+                    Tokens.add(Token);
+                
+            }
+            else //Else it is just a normal search input
+            {
+                //put the elements of in input in order and retrieve from the database in order 
+                //example :- "Search Engine Implementation"
+                //Get => Search followerd by Engine followed by Implementation
+                query = query.replaceAll("[^a-zA-Z ]|(  )+", "");           
+                String[] BodyArray = query.split(" ");                      // split words
+                
+                
+                List TokenList = Arrays.asList(BodyArray);
+
+                HashSet<String> TokensSet = new HashSet(TokenList);
+                HashSet<String> StemmedTokens = new HashSet();
+                TokensSet.removeAll(stopWords);
+                
+                for(String Token : TokensSet)      //stem words
+
+                    StemmedTokens.add(new Stemmer().stem(Token));
+                        
+                for(String Token : StemmedTokens)
+                    Tokens.add(Token);
+            }
+        }   
         
         return Tokens;        
     }
